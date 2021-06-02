b00a57c4120519f7e847328dcb7334d31e65ce2b
Jonas Ribeiro <jonasribeiro41@yahoo.com.br>
2021-06-01 22:12:06 -0300

    POST new Category

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -36,4 +36,12 @@ public class CategoryService {
 		return new CategoryDTo(entity);
 		
 	}
+    
+	@Transactional(readOnly = true)
+	public CategoryDTo insert(CategoryDTo dto) {
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new CategoryDTo(entity);
+	}
 }
