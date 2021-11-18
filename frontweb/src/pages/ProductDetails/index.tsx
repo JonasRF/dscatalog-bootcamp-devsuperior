a7a5ab3f6ffe71d8cc0cd5749d60e519f670752e
Jonas Ribeiro <jonasribeiro41@yahoo.com.br>
2021-11-18 16:01:46 -0300

    useState, useffect, useParams

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,39 +1,64 @@
-import { ReactComponent as ArrowIcon} from 'assets/images/arrow.svg';
-import ProductPrice from 'components/ProductPrice';
-import { Link } from 'react-router-dom';
+import { ReactComponent as ArrowIcon } from "assets/images/arrow.svg";
+import axios from "axios";
+import ProductPrice from "components/ProductPrice";
+import { Link, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Product } from "types/product";
+import { BASE_URL } from "util/requests";
 
-import './styles.css';
+import "./styles.css";
+
+type UrlParms = {
+    productId : string;
+}
 
 const ProductDetails = () => {
-    return (
-        <div className="base-card product-details-container">
-            <div className="product-details-card">
-                <Link to="/products">
-                <div className="goback-container">
-                    <ArrowIcon />
-                    <h2>VOLTAR</h2>
-                </div>
-                </Link>
-                <div className="row">
-                    <div className="col-xl-6">
-                        <div className="img-container">
-                            <img src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg" alt="nomedo producto"></img>
-                        </div>
-                        <div className="name-price-container">
-                            <h1>Nome do produto</h1>
-                            <ProductPrice price={2345.76}/>
-                        </div>
-                    </div>
-                    <div className="col-xl-6">
-                        <div className="description-container">
-                            <h2>Descrição do produto</h2>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, consectetur!</p>
-                        </div>
-                    </div>
-                </div>
+
+  const { productId } = useParams<UrlParms>();
+
+  const [product, setProduct] = useState<Product>();
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/products/${productId}`)
+    .then((response) => {
+      setProduct(response.data);
+    });
+  }, [productId]);
+
+  return (
+    <div className="base-card product-details-container">
+      <div className="product-details-card">
+        <Link to="/products">
+          <div className="goback-container">
+            <ArrowIcon />
+            <h2>VOLTAR</h2>
+          </div>
+        </Link>
+        <div className="row">
+          <div className="col-xl-6">
+            <div className="img-container">
+              <img
+                src={product?.imgUrl}
+                alt={product?.name}
+              ></img>
             </div>
+            <div className="name-price-container">
+              <h1>{product?.name}</h1>
+              {product && <ProductPrice price={product?.price} />}
+            </div>
+          </div>
+          <div className="col-xl-6">
+            <div className="description-container">
+              <h2>Descrição do produto</h2>
+              <p>
+               {product?.description}
+              </p>
+            </div>
+          </div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default ProductDetails;
