6d0122c65d4992fe987bdf601143b04a06c5b073
Jonas Ribeiro <jonasribeiro41@yahoo.com.br>
2021-11-29 11:07:01 -0300

    Saving auth to localStorage

--- frontweb/src/pages/Auth/Login/index.tsx
+++ frontweb/src/pages/Auth/Login/index.tsx
@@ -1,50 +1,72 @@
-import { Link } from 'react-router-dom';
-import ButtonIcon from 'components/ButtonIcon';
-import { useForm } from 'react-hook-form';
-import './styles.css';
-import { requestBackendLogin } from 'util/requests';
- 
+import { Link } from "react-router-dom";
+import ButtonIcon from "components/ButtonIcon";
+import { useForm } from "react-hook-form";
+import { getAuthData, requestBackendLogin, saveAuthData } from "util/requests";
+import { useState } from "react";
+
+import "./styles.css";
+
 type FormData = {
   username: string;
   password: string;
-}
+};
 
 const Login = () => {
+  const [hasError, setHasError] = useState(false);
 
-  const { register, handleSubmit } = useForm<FormData>();
+  const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
 
   const onSubmit = (formData: FormData) => {
-    console.log(formData)
+    console.log(formData);
     requestBackendLogin(formData)
-    .then(response => {
-      console.log('SUCESSO', response);
-    })
-      .catch(error => {
-        console.log('ERRO', error);
+      .then((response) => {
+        saveAuthData(response.data);
+        const token = getAuthData().access_token;
+        console.log('Token: ' + token);
+        setHasError(false);
+        console.log("SUCESSO", response);
+      })
+      .catch((error) => {
+        setHasError(true);
+        console.log("ERRO", error);
       });
   };
 
   return (
     <div className="base-card login-card">
       <h1>LOGIN</h1>
+      {hasError && (
+        <div className="alert alert-danger">Erro ao tentar efetuar o login</div>
+      )}
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-4">
-          <input 
-          {...register("username")}
+          <input
+            {...register("username", {
+              required: "Campo obrigatório",
+              pattern: {
+                value: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+                message: 'Email invalido'
+              }
+            })}
             type="text"
-            className="form-control base-input"
+            className={`form-control base-input ${errors.username ? 'is-invalid': ''}`} 
             placeholder="Email"
             name="username"
           />
+          <div className="invalid-feedback d-block">{errors.username?.message} </div>
         </div>
         <div className="mb-2">
           <input
-          {...register("password")}
+            {...register("password", {
+              required: 'Campo obrigatório'
+            })}
             type="password"
-            className="form-control base-input "
+            className={`form-control base-input ${errors.password ? 'is-invalid': ''}`}
             placeholder="Password"
             name="password"
           />
+         
+          <div className="invalid-feedback d-block">{errors.password?.message}</div>
         </div>
         <Link to="/admin/auth/recover" className="login-link-recover">
           Esqueci a senha
@@ -62,5 +84,5 @@ const Login = () => {
     </div>
   );
 };
- 
+
 export default Login;
