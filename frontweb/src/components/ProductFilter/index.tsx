026c41ff94ce1da20f8d7a6ae387ab2a0c5e10d6
Jonas Ribeiro <jonasribeiro41@yahoo.com.br>
2021-12-23 13:10:27 -0300

    React Rook Form to ProductFilter

--- frontweb/src/components/ProductFilter/index.tsx
+++ frontweb/src/components/ProductFilter/index.tsx
@@ -1,31 +1,77 @@
+import { ReactComponent as SearchIcon } from "assets/images/SearchIcon.svg";
+import { useEffect, useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import Select from "react-select";
+import { Category } from "types/category";
+import { requestBackend } from "util/requests";
 
-import { ReactComponent as SearchIcon } from 'assets/images/SearchIcon.svg';
+import "./styles.css";
 
-import './styles.css'
+type ProductFilterData = {
+  name: string;
+  category: Category;
+};
 
 const ProductFilter = () => {
-    return(
-        <div className="base-card product-filter-container"> 
-            <form className="product-filter-container">
-                <div className="product-filter-name-container">
-                    <input 
-                    type="text" 
-                    className="form-control"
-                    placeholder="Nome do produto"
-                     />
-                    <SearchIcon />
-                </div>
-                <div className="product-filter-botton-container">
-                   <div className="product-filter-category-container">
-                   <select name="" id="">
-                        <option value="">Livros</option>
-                    </select>
-                   </div>
-                    <button className="btn btn-outline-secondary">LIMPAR</button>
-                </div>
-            </form> 
-            </div>
-    )
-}
+
+  const [selectCategories, setSelectCAtegories] = useState<Category[]>([]);
+
+  const {
+    register,
+    handleSubmit,
+    control,
+  } = useForm<ProductFilterData>();
+
+  const onSubmit = (formData: ProductFilterData) => {
+    console.log("Sucesso", formData);
+  };
+
+  useEffect(() => {
+    requestBackend({ url: "/categories" }).then((response) => {
+      setSelectCAtegories(response.data.content);
+    });
+  });
+
+  return (
+    <div className="base-card product-filter-container">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="product-filter-container"
+      >
+        <div className="product-filter-name-container">
+          <input
+            {...register("name")}
+            type="text"
+            className="form-control"
+            placeholder="Nome do produto"
+            name="name"
+          />
+          <button>
+            <SearchIcon />
+          </button>
+        </div>
+        <div className="product-filter-botton-container">
+          <div className="product-filter-category-container">
+            <Controller
+              name="category"
+              control={control}
+              render={({ field }) => (
+                <Select
+                  {...field}
+                  options={selectCategories}
+                  isClearable
+                  classNamePrefix="product-crud-select"
+                  getOptionLabel={(category: Category) => category.name}
+                  getOptionValue={(category: Category) => String(category.id)}
+                />
+              )}
+            />
+          </div>
+          <button className="btn btn-outline-secondary">LIMPAR</button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default ProductFilter;
