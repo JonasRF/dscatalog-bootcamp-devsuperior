c401f364a1cf373b98800f5101e986cb373f4869
JonasRF <jonasribeiro41@yahoo.com.br>
2022-01-07 13:27:27 -0300

    Mocking function with spyOn

--- frontweb/src/util/__tests__/formatters.spec.ts
+++ frontweb/src/util/__tests__/formatters.spec.ts
@@ -6,7 +6,7 @@ describe('formatePrice for positive numbers', () => {
         const result = formatPrice(10.1);
         expect(result).toEqual("10,10");
     });
-
+  
     test('formatprice should format number pt-BR when given 0.1', () => {
         const result = formatPrice(0.1);
         expect(result).toEqual("0,10");
