369aab7d2478f65aca363e96c0b34dfc7ceeeb24
JonasRF <jonasribeiro41@yahoo.com.br>
2022-01-10 16:05:51 -0300

    catalog, waitFor, async-await

--- frontweb/src/components/Pagination/__tests__/Pagination.spec.tsx
+++ frontweb/src/components/Pagination/__tests__/Pagination.spec.tsx
@@ -1,17 +1,20 @@
 import { render, screen } from "@testing-library/react";
 import Pagination from "..";
+import userEvent from "@testing-library/user-event";
 
 describe('Pagination tests', () => {
 
-    test('', () => {
+    test('should render Pagination', () => {
 
        const pageCount = 3;
         const range = 3;
+        const onChange = jest.fn();
 
         render(
             <Pagination 
             pageCount = {pageCount}
             range = {range}
+            onChange={onChange}
             />
         );
 
@@ -32,4 +35,67 @@ describe('Pagination tests', () => {
         expect(page4).not.toBeInTheDocument();
 
     });
+
+    test('next arrow should call onChange', () => {
+
+        const pageCount = 3;
+         const range = 3;
+         const onChange = jest.fn();
+ 
+         render(
+             <Pagination 
+             pageCount = {pageCount}
+             range = {range}
+             onChange={onChange}
+             />
+         );
+
+         const arrowNext = screen.getByTestId("arrow-next");
+
+         userEvent.click(arrowNext);
+        expect(onChange).toHaveBeenCalledWith(1);
+     });
+  
+    test('Previous arrow should call onChange', () => {
+
+        const pageCount = 3;
+         const range = 3;
+         const onChange = jest.fn();
+         const forcePage = 1;
+ 
+         render(
+             <Pagination 
+             pageCount = {pageCount}
+             range = {range}
+             onChange={onChange}
+             forcePage={forcePage}
+             />
+         );
+
+         const arrowPrevious = screen.getByTestId("arrow-previous");
+
+         userEvent.click(arrowPrevious);
+        expect(onChange).toHaveBeenCalledWith(0);
+ 
+     });
+
+     test('Page link should call onChange', () => {
+
+        const pageCount = 3;
+         const range = 3;
+         const onChange = jest.fn();
+ 
+         render(
+             <Pagination 
+             pageCount = {pageCount}
+             range = {range}
+             onChange={onChange}
+             />
+         );
+
+         const page2 = screen.getByText('2');
+
+         userEvent.click(page2);
+         expect(onChange).toHaveBeenCalledWith(1);
+     });
 });
