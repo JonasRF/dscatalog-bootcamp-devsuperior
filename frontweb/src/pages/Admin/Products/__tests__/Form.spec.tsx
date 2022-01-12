d5d29292e4fa41f76855b5489f9718457b316a3f
JonasRF <jonasribeiro41@yahoo.com.br>
2022-01-12 12:25:40 -0300

    Test update Should show toast and redirect when submit form correctly

--- frontweb/src/pages/Admin/Products/__tests__/Form.spec.tsx
+++ frontweb/src/pages/Admin/Products/__tests__/Form.spec.tsx
@@ -3,7 +3,7 @@ import userEvent from "@testing-library/user-event";
 import { Router, useParams } from "react-router-dom";
 import history from "util/history";
 import Form from "../Form";
-import { server } from "./fixtures";
+import { ProductResponse, server } from "./fixtures";
 import selectEvent from "react-select-event";
 import { ToastContainer } from "react-toastify";
 
@@ -24,7 +24,7 @@ describe('Product form create tests', () => {
         })
     })
 
-   test ('Should show toast and redirect when submit form correctly', async () => {
+    test('Should show toast and redirect when submit form correctly', async () => {
 
         render(
             <Router history={history}>
@@ -32,14 +32,14 @@ describe('Product form create tests', () => {
                 <ToastContainer />
             </Router>
         );
-        
+
         const nameInput = screen.getByTestId('name');
         const priceInput = screen.getByTestId('price');
         const imgURLInput = screen.getByTestId('imgUrl');
         const descriptionInput = screen.getByTestId('description');
         const categoriesInput = screen.getByLabelText('Categorias');
 
-        const submitButton = screen.getByRole('button', { name: /salvar/i})
+        const submitButton = screen.getByRole('button', { name: /salvar/i })
 
         await selectEvent.select(categoriesInput, ['Eletrônicos', 'Computadores']);
         userEvent.type(nameInput, 'Computador');
@@ -50,14 +50,14 @@ describe('Product form create tests', () => {
         userEvent.click(submitButton);
 
         await waitFor(() => {
-        const toastElement = screen.getByText('Produto cadastrado com sucesso');
-        expect(toastElement).toBeInTheDocument();
+            const toastElement = screen.getByText('Produto cadastrado com sucesso');
+            expect(toastElement).toBeInTheDocument();
         });
 
         expect(history.location.pathname).toEqual('/admin/products');
     });
 
-    test ('Should show 5 validation messages when just clicking submit', async () => {
+    test('Should show 5 validation messages when just clicking submit', async () => {
 
         render(
             <Router history={history}>
@@ -65,7 +65,7 @@ describe('Product form create tests', () => {
             </Router>
         );
 
-        const submitButton = screen.getByRole('button', { name: /salvar/i});
+        const submitButton = screen.getByRole('button', { name: /salvar/i });
 
         userEvent.click(submitButton);
 
@@ -74,5 +74,87 @@ describe('Product form create tests', () => {
             expect(messages).toHaveLength(5);
         })
     });
+
+    test('Should clear validation messages when filling out the form correctly', async () => {
+
+        render(
+            <Router history={history}>
+                <Form />
+            </Router>
+        );
+
+        const submitButton = screen.getByRole('button', { name: /salvar/i });
+
+        userEvent.click(submitButton);
+
+        await waitFor(() => {
+            const messages = screen.getAllByText('Campo obrigatório')
+            expect(messages).toHaveLength(5);
+        })
+
+        const nameInput = screen.getByTestId('name');
+        const priceInput = screen.getByTestId('price');
+        const imgURLInput = screen.getByTestId('imgUrl');
+        const descriptionInput = screen.getByTestId('description');
+        const categoriesInput = screen.getByLabelText('Categorias');
+
+        await selectEvent.select(categoriesInput, ['Eletrônicos', 'Computadores']);
+        userEvent.type(nameInput, 'Computador');
+        userEvent.type(priceInput, '5000.12');
+        userEvent.type(imgURLInput, 'https://i.dell.com/is/image/DellContent//content/dam/global-asset-library/Products/Enterprise_Servers/PowerEdge/t140/dellemc_pet140_bezel_lf.psd?fmt=pjpg&pscan=auto&scl=1&wid=1738&hei=1894&qlt=100,0&resMode=sharp2&size=1738,1894');
+        userEvent.type(descriptionInput, 'Excelente esse computador');
+
+        await waitFor(() => {
+            const messages = screen.queryAllByText('Campo obrigatório')
+            expect(messages).toHaveLength(0);
+        })
+    });
 });
 
+describe('Product form update tests', () => {
+
+    beforeEach(() => {
+        (useParams as jest.Mock).mockReturnValue({
+            productId: '2'
+        });
+    });
+
+    test('Should show toast and redirect when submit form correctly', async () => {
+
+        render(
+            <Router history={history}>
+                <Form />
+                <ToastContainer />
+            </Router>
+        );
+
+        await waitFor(() => {
+            const nameInput = screen.getByTestId('name');
+            const priceInput = screen.getByTestId('price');
+            const imgURLInput = screen.getByTestId('imgUrl');
+            const descriptionInput = screen.getByTestId('description');
+
+            const formElement = screen.getByTestId('form');
+
+            expect(nameInput).toHaveValue(ProductResponse.name);
+            expect(priceInput).toHaveValue(String(ProductResponse.price));
+            expect(imgURLInput).toHaveValue(ProductResponse.imgUrl);
+            expect(descriptionInput).toHaveValue(ProductResponse.description);
+
+            const ids = ProductResponse.categories.map(x => String(x.id));
+            expect(formElement).toHaveFormValues({categories: ids});
+
+        });
+
+        const submitButton = screen.getByRole('button', { name: /salvar/i });
+
+        userEvent.click(submitButton);
+
+        await waitFor(() => {
+            const toastElement = screen.getByText('Produto cadastrado com sucesso');
+            expect(toastElement).toBeInTheDocument();
+        });
+
+        expect(history.location.pathname).toEqual('/admin/products');
+    });
+});
